67e354b5172a86babb2dac7b6ac913e4d0ae4ef4
Radu Stoica <radu.stoica1994@gmail.com>
2020-08-21 22:34:22 +0200

    download sketch files

--- src/download-card/download-card.component.ts
+++ src/download-card/download-card.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./download-card.component.css"]
 })
 export class DownloadCardComponent implements OnInit {
-  // @Input() hero: Hero;
+  selectedFile: String = "Select one";
+
+  files: any[] = [
+    {
+      fileName: "day_1159.sketch",
+      option: "Day 1159"
+    },
+    {
+      fileName: "day_1160.sketch",
+      option: "Day 1160"
+    },
+    {
+      fileName: "day_1164.sketch",
+      option: "Day 1164"
+    }
+  ];
+
+  downloadFile() {
+    if ("Select one" === this.selectedFile) return;
+
+    let link = document.createElement("a");
+    link.setAttribute("type", "hidden");
+    link.href = `../assets/${this.selectedFile}`;
+    link.download = `filename`;
+    link.target = "_blank";
+    document.body.querySelector("app-root").append(link);
+    link.click();
+    link.remove();
+  }
 
   ngOnInit() {}
 }
--- src/download-card/download-card.module.ts
+++ src/download-card/download-card.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 import { DownloadCardComponent } from "./download-card.component";
 
 @NgModule({
   declarations: [DownloadCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [DownloadCardComponent]
 })
 export class DownloadCardModule {}
